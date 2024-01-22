5de6cdae193bb6f4a2e3700fd04c7f33d4bde69a
VILLAMORGO <villamorgo@gmail.com>
2024-01-23 02:18:33 +0800

    Try absolute path

--- script/intro-section-loader.js
+++ script/intro-section-loader.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const container = document.getElementById('intro-section');
 
-    fetch('../partials/_intro-section.html')
+    fetch('./../partials/_intro-section.html')
         .then(response => response.text())
         .then(data => {
             // Inject the partial content into the container
