59f6247f9190d186e445dab478a81623a65dcc85
VILLAMORGO <villamorgo@gmail.com>
2024-01-23 03:02:53 +0800

    fix url

--- script/partial-nav-loader.js
+++ script/partial-nav-loader.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const container = document.getElementById('header-container');
 
-    fetch('./../partials/_navbar.html')
+    fetch('/partials/_navbar.html')
         .then(response => response.text())
         .then(data => {
             // Inject the partial content into the container
